ac7b80b31dd44d4623dd85875327c55dcd8c1615
LuisQuintino <49841425+LuisQuintino@users.noreply.github.com>
2022-03-16 12:02:58 -0300

    Delete conversaoKM.py

--- conversaoKM.py
+++
@@ -1,5 +0,0 @@
-#Luis Quintino - Programa para calcular Distancia e velocidade média em unidade de tempo.
-d = float(input("Distância em KM: "))
-vm = float(input("Velocidade Média: "))
-tempo =  d // vm
-print("vai demorar esse montão de tempo aqui o", tempo, " hrs")
