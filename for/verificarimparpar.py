e983c6dd823262a12499775b3b5a6f7cdddedb19
LuisQuintino <49841425+LuisQuintino@users.noreply.github.com>
2022-03-16 12:29:21 -0300

    Add files via upload

---
+++ for/verificarimparpar.py
@@ -0,0 +1,6 @@
+#Luis Quintino - Programa para verificar se é impar ou par os números entre 1 e 15.
+for i in range(1, 16):
+  if i % 2 == 0:
+    print(i, "é par")
+  else:
+    print(i, "é impar")
