1df4000c7afb6cc800480d04e676be1438e33b37
LuisQuintino <49841425+LuisQuintino@users.noreply.github.com>
2022-03-16 12:02:52 -0300

    Delete AumentoSalarial.py

--- AumentoSalarial.py
+++
@@ -1,5 +0,0 @@
-#Luis Quintino - Programa que calcula o aumento salarial através de porcentagem.
-sal = float(input("Sua renda> "))
-por = float(input("Porcentagem do aumento> "))
-aumento = sal * (por / 100)
-print("Sua nova renda= ", sal + aumento)
